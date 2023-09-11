7468de9b7ca60e2d4bcf7c4bad2fbb56fdb1e022
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 09:49:55 -0300

    Adicionando arquivos

---
+++ projeto1/src/Home.jsx
@@ -0,0 +1,12 @@
+import {} from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à página Home</h1>
+      <p>Esta é a página inicial do nosso site.</p>
+    </div>
+  );
+}
+
+export default Home;
